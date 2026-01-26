c4ca4e033c25a4bfc2da4275c89c76c7fa92b657
Michael Mussato <michimussato@gmail.com>
2026-01-26 12:05:32 +0100

    - replace str_gen with get_docs()

--- src/OpenStudioLandscapes/n8n/config/models.py
+++ src/OpenStudioLandscapes/n8n/config/models.py
@@ -11,7 +11,6 @@ from pydantic import (
 LOGGER = get_dagster_logger(__name__)
 
 from OpenStudioLandscapes.engine.config.models import FeatureBaseModel
-from OpenStudioLandscapes.engine.config.str_gen import get_config_str
 
 from OpenStudioLandscapes.n8n import constants, dist
 
@@ -116,6 +115,5 @@ class Config(FeatureBaseModel):
         return ret
 
 
-CONFIG_STR = get_config_str(
-    Config=Config,
-)
+CONFIG_STR = Config.get_docs()
+
