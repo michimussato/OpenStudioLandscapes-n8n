f39fc3a6246775dc124f73875b5d1ccee5a62871
Michael Mussato <michimussato@gmail.com>
2026-04-26 17:52:02 +0200

    add computed_field to properties

--- src/OpenStudioLandscapes/n8n/config/models.py
+++ src/OpenStudioLandscapes/n8n/config/models.py
@@ -6,6 +6,7 @@ from dagster import get_dagster_logger
 from pydantic import (
     Field,
     PositiveInt,
+    computed_field,
 )
 
 LOGGER = get_dagster_logger(__name__)
@@ -96,6 +97,7 @@ class Config(FeatureBaseModel):
     # )
 
     # EXPANDABLE PATHS
+    @computed_field
     @property
     def n8n_volume_expanded(self) -> pathlib.Path:
         LOGGER.debug(f"{self.env = }")
