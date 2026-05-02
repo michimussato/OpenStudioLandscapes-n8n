ab0be4d75fd3d4ed6cd91b65b02a69c0129ad4ae
Michael Mussato <michimussato@gmail.com>
2026-05-02 13:11:30 +0200

    add better logging

--- src/OpenStudioLandscapes/n8n/config/models.py
+++ src/OpenStudioLandscapes/n8n/config/models.py
@@ -2,16 +2,12 @@ import enum
 import pathlib
 from typing import List
 
-from dagster import get_dagster_logger
 from pydantic import (
     Field,
     PositiveInt,
 )
 
-from OpenStudioLandscapes.cli import LOGGING_LEVEL_DEFAULT
-
-LOGGER = get_dagster_logger(__name__)
-LOGGER.setLevel(LOGGING_LEVEL_DEFAULT)
+from OpenStudioLandscapes.engine.logging.loggers import FEATURE_LOGGER as LOGGER
 
 from OpenStudioLandscapes.engine.config.models import FeatureBaseModel
 
